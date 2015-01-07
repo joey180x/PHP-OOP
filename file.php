c79350e4bf4a5e077a0b4a6b0dff42a743633db9
Joseph De La Portilla <joey180x@gmail.com>
2015-01-07 11:41:17 -0800

    php oop

---
+++ file.php
@@ -0,0 +1,65 @@
+<?php
+
+class Kim {
+public $firstName = “default name”;
+public $lastName = “default last name”;
+public $gender = “male”;
+public $price = 0;
+function getName() {
+return “{$this>
+firstName}” .
+“{$this>
+lastName}”;
+}
+}
+$Kim1 = new Kim();
+$Kim1>
+firstName = “Kim”;
+$Kim1>
+lastName = “Jong-Un”;
+print “The Persons’s name is {$People1>getName()}.”;
+
+
+
+class Ryan {
+public $firstName = “default name”;
+public $lastName = “default last name”;
+public $gender = “male”;
+public $price = 0;
+function getName() {
+return “{$this>
+firstName}” .
+“{$this>
+lastName}”;
+}
+}
+$Ryan1 = new People();
+$Ryan1>
+firstName = “Ryan”;
+$Ryan1>
+lastName = “Ryanian”;
+print “The humans name is {$Ryan1>getName()}.”;
+
+
+
+class Luke {
+public $firstName = “default name”;
+public $lastName = “default last name”;
+public $gender = “male”;
+public $price = 0;
+function getName() {
+return “{$this>
+firstName}” .
+“{$this>
+lastName}”;
+}
+}
+$Luke1 = new Luke();
+$Luke1>
+firstName = “Luke”;
+$Luke1>
+lastName = “Lukian”;
+print “The Persons’s name is {$Luke1>getName()}.”;
+
+
+?>
