ef38bdbc1557c5b4ab1a02c5e5247f8a514d3d31
Joseph De La Portilla <joey180x@gmail.com>
2015-01-07 11:58:00 -0800

    cleaned it up a bit

--- file.php
+++ file.php
@@ -1,17 +1,16 @@
 <?php
 
 class Kim {
-public $firstName = “default name”;
-public $lastName = “default last name”;
-public $gender = “male”;
-public $price = 0;
+	public $firstName = “default name”;
+	public $lastName = “default last name”;
+	public $gender = “male”;
+	public $price = 0;
 function getName() {
-return “{$this>
-firstName}” .
-“{$this>
-lastName}”;
-}
-}
+			return “{$this>firstName}” .“{$this>lastName}”;
+			
+			}
+	}
+
 $Kim1 = new Kim();
 $Kim1>
 firstName = “Kim”;
@@ -20,19 +19,17 @@ lastName = “Jong-Un”;
 print “The Persons’s name is {$People1>getName()}.”;
 
 
-
 class Ryan {
-public $firstName = “default name”;
-public $lastName = “default last name”;
-public $gender = “male”;
-public $price = 0;
+		public $firstName = “default name”;
+		public $lastName = “default last name”;
+		public $gender = “male”;
+		public $price = 0;
 function getName() {
-return “{$this>
-firstName}” .
-“{$this>
-lastName}”;
-}
-}
+				return “{$this>firstName}” .“{$this>lastName}”;
+			
+			}
+	}
+
 $Ryan1 = new People();
 $Ryan1>
 firstName = “Ryan”;
@@ -43,21 +40,18 @@ print “The humans name is {$Ryan1>getName()}.”;
 
 
 class Luke {
-public $firstName = “default name”;
-public $lastName = “default last name”;
-public $gender = “male”;
-public $price = 0;
+		public $firstName = “default name”;
+		public $lastName = “default last name”;
+		public $gender = “male”;
+		public $price = 0;
 function getName() {
-return “{$this>
-firstName}” .
-“{$this>
-lastName}”;
-}
-}
+			return “{$this>firstName}” .“{$this>lastName}”;
+			
+			}
+	}
 $Luke1 = new Luke();
 $Luke1>
-firstName = “Luke”;
-$Luke1>
+firstName = “Luke”;$Luke1>
 lastName = “Lukian”;
 print “The Persons’s name is {$Luke1>getName()}.”;
 
